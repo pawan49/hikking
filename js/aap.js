1dce2d7658607832562482bfd5e14492d50bb15e
pawan49 <pawan49cs@gmail.com>
2020-12-13 07:28:33 +0530

    changes

--- js/aap.js
+++ js/aap.js
@@ -1,5 +1,5 @@
 let navTrigger = document.querySelector('.nav-trigger');
-let siteContentWrapper = document.querySelector('.site-content-wrapper')
+let siteContentWrapper = document.querySelector('.site-content-wrapper');
 navTrigger.addEventListener('click', function() {
     siteContentWrapper.classList.toggle('scaled')
-})
+});
